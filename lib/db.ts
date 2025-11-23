74cccf7aebd1d2e0e1a7f7ea3e42e8189e74d8b7
Dave Brown <dfbrownnz@gmail.com>
2025-11-23 11:58:44 -0500

    db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -10,23 +10,23 @@
 
 // lib/db.ts
 
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
 // 1. Declare a global variable to store the PrismaClient instance
 //    This prevents multiple instantiations in development.
-const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+//const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
 // 2. Initialize the client only if it hasn't been initialized yet.
 //    In production, this just runs once.
 //    In development, it uses the existing global instance on HMR.
-export const prisma = 
-  globalForPrisma.prisma ?? new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'], // Recommended logging options
-  });
+export const prisma = []
+  // globalForPrisma.prisma ?? new PrismaClient({
+  //   log: ['query', 'info', 'warn', 'error'], // Recommended logging options
+  // });
 
 // 3. In development, save the instance to the global object.
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   globalForPrisma.prisma = prisma;
+// }
 
 export default prisma;
